ce8c0e29f847c898649dec4ebf9e2c11b324abb6
yaniv1223 <157611742+yaniv1223@users.noreply.github.com>
2025-05-22 23:38:59 +0300

    Update main.jsx
    change the logo path

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import logo from 'C:/Users/admin/Documents/GitHub/Math_Game/public/logo.svg'
+import logo from '../public/logo.svg' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
